07e0a7ff42b26602d65fc21d0542c71fc4cc5c7c
Chitrangda12 <151382342+Chitrangda12@users.noreply.github.com>
2026-03-31 19:57:49 +0530

    Update reviewRoutes.js

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const { createReview, getServiceReviews } = require("../controllers/reviewController");
 const { protect } = require("../middleware/authMiddleware");
 
-// add review
 router.post("/", protect, createReview);
 
-// get reviews of a service
 router.get("/:serviceId", getServiceReviews);
 
 module.exports = router;
