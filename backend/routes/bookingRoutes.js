2968c3db626cef2f264cfa093e4d593cf60b88d0
Chitrangda12 <151382342+Chitrangda12@users.noreply.github.com>
2026-03-31 19:57:23 +0530

    Update bookingRoutes.js

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -10,16 +10,12 @@ const {
 
 const { protect, authorizeRoles } = require("../middleware/authMiddleware");
 
-// user books
 router.post("/", protect, createBooking);
 
-// user bookings
 router.get("/my", protect, getMyBookings);
 
-// provider bookings
 router.get("/provider", protect, authorizeRoles("provider"), getProviderBookings);
 
-// update booking (accept/reject)
 router.put("/:id", protect, authorizeRoles("provider"), updateBookingStatus);
 
 module.exports = router;
