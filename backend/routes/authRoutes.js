0c0ac6ed5a1483447c48b50f6e8e31901275422c
Chitrangda12 <151382342+Chitrangda12@users.noreply.github.com>
2026-03-31 19:56:55 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { signupUser, loginUser } = require("../controllers/authController");
 
-// signup route
 router.post("/signup", signupUser);
-// login route
+
 router.post("/login", loginUser);
 
 module.exports = router;
