9a948494a86388f9142242cb0d8d59b53aa7e38c
Chitrangda12 <151382342+Chitrangda12@users.noreply.github.com>
2026-03-31 19:58:07 +0530

    Update serviceRoutes.js

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const { createService, getServices } = require("../controllers/serviceController");
 const { protect, authorizeRoles } = require("../middleware/authMiddleware");
 
-// only providers can create
 router.post("/", protect, authorizeRoles("provider"), createService);
 
-// anyone logged in can view
 router.get("/", protect, getServices);
 
 module.exports = router;
